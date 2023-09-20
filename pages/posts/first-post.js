fcc262896a91615277e2e7e5b7b61c88ee23264d
LeinadCybre1024 <leinadcybre@gmail.com>
2023-09-20 22:36:34 +0300

    Assets, Mettadata and css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,29 @@
+import styles from "../../styles/Home.module.css";
+import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../..//components/layout";
+
 export default function Firstpost() {
-    return <h1>Firstpost</h1>
-};
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+          <Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"
+            onLoad={() =>
+              console.log(
+                `script loaded correctly, window.FB has been populated`
+              )
+            }
+          />
+        </Head>
+        <h1 className={styles.title}>
+          First Page <Link href="/">this page!</Link>
+        </h1>
+      </Layout>
+    </>
+  );
+}
