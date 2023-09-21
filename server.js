5a3213ccbc55981d88befb68853d52b5fa12ce8a
LeinadCybre1024 <leinadcybre@gmail.com>
2023-09-21 22:12:29 +0300

    dynami routing ,, faced fs error

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const next = require("next");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const fs = require("fs");
+
+app.prepare().then(() => {
+  const server = express();
+
+  // You can define custom routes here, e.g.:
+  // server.get('/my-route', (req, res) => res.send('Hello World'));
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
